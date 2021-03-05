3081d372fff2b2e00e7681adf25f15a327508c79
taurusilver7 <prasad20697@gmail.com>
2021-03-05 20:12:25 +0530

    induvidual chat component created

---
+++ tinder-client/src/components/Chats/Chat/Chat.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Chat.css";
+
+import { Avatar } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+const Chat = ({ name, message, timestamp, profilePic }) => {
+  return (
+    <Link to={`/chat/${name}`}>
+      <div className="chat">
+        <Avatar className="chat__image" alt={name[0]} src={profilePic} />
+        <div className="chat__info">
+          <h2>{name}</h2>
+          <p>{message}</p>
+        </div>
+        <p className="chat__timestamp">{timestamp}</p>
+      </div>
+    </Link>
+  );
+};
+
+export default Chat;
